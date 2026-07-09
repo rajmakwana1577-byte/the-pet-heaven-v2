a15145ae18a0f078b7cbfc9a31629bd74aee4486
rajmakwana1577-byte <rajmakwana1577@gmail.com>
2026-07-09 19:11:01 +0530

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "pet-heaven-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/maskable-icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
